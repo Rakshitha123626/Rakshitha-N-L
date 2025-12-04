a5bf163f18cc46d288064ff7dd21506b361be806
Rakshitha123626 <rakshithanl097@gmail.com>
2025-12-04 18:26:05 +0530

    Add files via upload

---
+++ Multiplesofarray.java
@@ -0,0 +1,36 @@
+package com.acharya.classes;
+import java.util.Scanner;
+
+public class Multiplesofarray {
+
+   // User-defined method
+    static void printMultiples(int arr[]) {
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 5 == 0) {
+                count++;
+            }
+        }
+
+        System.out.println("Output: " + count);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+
+        printMultiples(arr); // calling method
+    }
+}
