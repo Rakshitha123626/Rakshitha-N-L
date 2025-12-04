c0f0872378b833c3191694ba21e58bdad041acd7
Rakshitha123626 <rakshithanl097@gmail.com>
2025-12-04 19:14:05 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,20 @@
+package Program;
+
+public class swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=25;
+		int b=30;
+		int temp;
+		System.out.println("before swapping value of a="+a);
+		System.out.println("before swapping value of b="+b);
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println("after swapping "+a);
+		System.out.println("after swapping value of b"+b);
+
+	}
+
+}
