9ad50ba71e5c2aab5d390ea3f71185839822e03d
Rakshitha123626 <rakshithanl097@gmail.com>
2025-12-04 19:09:10 +0530

    Add files via upload

---
+++ Pizza.java
@@ -0,0 +1,39 @@
+package Program;
+import java.util.Scanner;
+public class Pizza {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Scanner sc=new Scanner(System.in);
+    int amount=0;
+    System.out.println("ENter the size of pizza");
+    String size=sc.nextLine();
+    switch(size)
+    {
+    case "small":
+    		
+    	
+     System.out.println(amount=125);
+     break;
+    
+    case "medium":
+    	
+    	System.out.println(amount=250);
+    	 break;
+    	 
+    case "large":
+    	
+    	System.out.println(amount=300);
+    	break;
+    	
+    case "very large":
+    	
+    	System.out.println(amount=350);
+    	break;
+    	default:
+    		 System.out.println("wrong size");
+    }
+    
+	}
+
+}
