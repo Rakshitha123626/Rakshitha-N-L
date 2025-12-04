2f673e0bd4a55aaf6484c6128fb943db645c2578
Rakshitha123626 <rakshithanl097@gmail.com>
2025-12-04 19:04:40 +0530

    Add files via upload

---
+++ leapyear.java
@@ -0,0 +1,22 @@
+package Program;
+import java.util.Scanner;
+public class leapyear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+    System.out.println("enter the year");
+    int year=sc.nextInt();
+    if((year%4)==0)
+    {
+    	System.out.println("leap year");
+    }
+    else
+    {
+    	System.out.println( "not a leap year");
+    }
+    	
+    
+	}
+
+}
