058c71491d4188ebb58f86d8a914bf5d420508c7
Rakshitha123626 <rakshithanl097@gmail.com>
2025-12-04 18:59:42 +0530

    Add files via upload

---
+++ SumofFourNumber.java
@@ -0,0 +1,21 @@
+package Com.acharya.com;
+
+public class SumofFourNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number1 = 10;
+		int number2 = 10;
+
+		int sum = number1 + number2;
+		System.out.println("Sum of the TwoNumber is  " + sum);
+		int number3 = 15;
+		sum = sum + number3;
+		System.out.println("sum of Three NUmber is "+sum);
+		int number4 = 25;
+		sum = sum + number4;
+		System.out.println("Sum of the Four Number is  " + sum);
+		
+	}
+
+}
