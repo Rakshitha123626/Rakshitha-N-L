037da8f4e483c66cf9181f985d4e0392f4bd54db
Rakshitha123626 <rakshithanl097@gmail.com>
2025-12-05 20:31:53 +0530

    FindingPrimeOrNOt

---
+++ PrimeOrNot.java
@@ -0,0 +1,33 @@
+package Com.acharya.Classes;
+
+import java.util.Scanner;
+
+public class PrimeOrNot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int count = 0;
+
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+
+        if (count == 2) {
+            System.out.println("Prime number");
+        } else {
+            System.out.println("Not a prime number");
+        }
+    }
+
+
+
+	}
+
+
